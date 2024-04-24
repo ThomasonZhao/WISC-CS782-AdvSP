490b9a6633c13a5cfa81ad0787dff022cdc4ab4f
Thomason Zhao <thomasonzhao@outlook.com>
2024-04-23 21:31:40 -0500

    stress test script finished

--- project/TEST/main.js
+++ project/TEST/main.js
@@ -1,40 +1,44 @@
-// main.js
-const { Worker } = require('worker_threads');
-const path = require('path');
+import { Worker } from "worker_threads";
 
-const numThreads = 10; // Number of worker threads to create
+const numThreads = 16; // Number of worker threads to create
 const numRounds = 10; // Number of rounds per worker
+// const protocol = "BASIC"; // Protocol to use for registration
+// const protocol = "SRP"; // Protocol to use for registration
+const protocol = "OPAQUE"; // Protocol to use for registration
 
 // Function to create a worker thread
 function createWorker(index) {
-  const worker = new Worker(path.resolve(__dirname, 'worker.js'), {
-    env: { WORKER_ID: index },
+  const worker = new Worker("./worker.js", {
+    workerData: {
+      workerIndex: index,
+      numRounds,
+      protocol,
+    },
   });
   return worker;
 }
 
 // Function to perform the performance test
-function runPerformanceTest() {
+async function runPerformanceTest() {
   const startTime = performance.now();
   const workers = Array.from({ length: numThreads }, (_, index) => createWorker(index));
 
   // Start the workers and wait for them to finish
-  const workerPromises = workers.map((worker, index) => {
+  const workerPromises = workers.map((worker) => {
     return new Promise((resolve) => {
-      worker.on('message', (event) => {
-        if (event.type === 'finished') {
+      worker.on("message", (event) => {
+        if (event.type === "finished") {
           resolve();
         }
       });
-      worker.postMessage({ type: 'start', payload: { rounds: numRounds, workerIndex: index } });
+      worker.postMessage({ type: "start" });
     });
   });
 
-  Promise.all(workerPromises).then(() => {
-    const endTime = performance.now();
-    console.log(`Performance test completed in ${endTime - startTime} milliseconds.`);
-    process.exit();
-  });
+  await Promise.all(workerPromises);
+  const endTime = performance.now();
+  console.log(`${protocol} performance test completed in ${(endTime - startTime) / 1000} seconds.`)
+  process.exit();
 }
 
 runPerformanceTest();
